55f25393c8538910418d529f21edda7d157089f7
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2021-07-20 14:16:34 -0500

    created server

--- App.js
+++ App.js
@@ -6,12 +6,43 @@ import { NavigationContainer } from "@react-navigation/native";
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
 
-    </NavigationContainer>
-  );
+
+export default function App() {
+  const [state, setState] = useState({
+    token: null,
+  });
+
+  React.useEffect(() => {
+    (async () => {
+      let userToken = await AsyncStorage.getItem("userToken");
+
+      setState({ token: userToken });
+    })();
+  }, []);
+
+  return (!state.token ?
+    (<NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='loginPage'
+          component={Login}
+          options={{ title: "Login" }}
+        />
+        <Stack.Screen
+          name='signUp'
+          component={SignUp}
+          options={{ title: "Sign Up" }}
+        />
+        <Stack.Screen name='home' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>) :
+    (<NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='home' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>)
+  )
 }
 
 
